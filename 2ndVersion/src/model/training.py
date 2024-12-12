006995e2e5ec0bebaedc0e049fc2ceeca291d8ed
jinxIsOp <154748750+jinxIsOp@users.noreply.github.com>
2024-12-13 00:41:55 +0100

    idk tbh

--- 2ndVersion/src/model/training.py
+++ 2ndVersion/src/model/training.py
@@ -12,35 +12,31 @@ class ChessTrainer:
         states = np.array(states)
         target_policies = np.array(target_policies)
         target_values = np.array(target_values)
-        
+
         states = torch.FloatTensor(states).to(self.device)
         target_policies = torch.FloatTensor(target_policies).to(self.device)
         target_values = torch.FloatTensor(target_values).to(self.device)
         
-        # Process in batches
         total_loss = 0
-        for i in range(0, len(states), batch_size):
-            batch_states = states[i:i+batch_size]
-            batch_policies = target_policies[i:i+batch_size]
-            batch_values = target_values[i:i+batch_size]
+        num_batches = (len(states) + batch_size - 1) // batch_size
+        
+        for i in range(num_batches):
+            start_idx = i * batch_size
+            end_idx = min(start_idx + batch_size, len(states))
+            
+            batch_states = states[start_idx:end_idx]
+            batch_policies = target_policies[start_idx:end_idx]
+            batch_values = target_values[start_idx:end_idx]
             
             self.optimizer.zero_grad()
             policy_out, value_out = self.model(batch_states)
             
             policy_loss = -torch.mean(torch.sum(batch_policies * policy_out, dim=1))
             value_loss = torch.mean((value_out - batch_values) ** 2)
-            total_loss = policy_loss + value_loss
+            loss = policy_loss + value_loss
             
-            total_loss.backward()
+            loss.backward()
             self.optimizer.step()
+            total_loss += loss.item()
         
-        return total_loss.item()
-
-    def train(self, train_loader, num_epochs):
-        for epoch in range(num_epochs):
-            total_loss = 0
-            for batch_idx, (states, policies, values) in enumerate(train_loader):
-                loss = self.train_step(states, policies, values)
-                total_loss += loss
-                
-            print(f"Epoch {epoch+1}, Average Loss: {total_loss/len(train_loader)}")
+        return total_loss / num_batches
